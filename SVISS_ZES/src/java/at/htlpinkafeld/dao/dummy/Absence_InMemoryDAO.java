179f001015a162198fc44e0881cb941024bbe554
ProEnduro <jens.wunderler@gmail.com>
2016-08-03 16:25:53 +0200

    More Absence moree!!!
    :dancer:

--- SVISS_ZES/src/java/at/htlpinkafeld/dao/dummy/Absence_InMemoryDAO.java
+++ SVISS_ZES/src/java/at/htlpinkafeld/dao/dummy/Absence_InMemoryDAO.java
@@ -29,12 +29,18 @@ public class Absence_InMemoryDAO extends Base_InMemoryDAO<Absence> implements Ab
         super.insert(new Absence(0, uDAO.getUser(1), aDAO.getAbsenceTypeByID(1), LocalDateTime.of(2016, 4, 4, 0, 0, 0), LocalDateTime.of(2016, 4, 6, 0, 0, 0), "Cold", true));
         super.insert(new Absence(uDAO.getUser(2), aDAO.getAbsenceTypeByID(3), LocalDateTime.of(2016, 4, 4, 0, 0, 0), LocalDateTime.of(2016, 4, 6, 0, 0, 0)));
         
-        super.insert(new Absence(uDAO.getUser(9), aDAO.getAbsenceTypeByID(4), LocalDateTime.of(2016, 7, 8, 8, 0, 0), LocalDateTime.of(2016, 7, 8, 13, 30, 0)));
+        super.insert(new Absence(0, uDAO.getUser(9), aDAO.getAbsenceTypeByID(4), LocalDateTime.of(2016, 7, 8, 8, 0, 0), LocalDateTime.of(2016, 7, 8, 13, 30, 0), "Arrow in the knee!",true));
         super.insert(new Absence(uDAO.getUser(5), aDAO.getAbsenceTypeByID(1), LocalDateTime.of(2016, 9, 30, 0, 0, 0), LocalDateTime.of(2016, 9, 30, 0, 0, 0)));
         super.insert(new Absence(uDAO.getUser(7), aDAO.getAbsenceTypeByID(2), LocalDateTime.of(2016, 2, 12, 0, 0, 0), LocalDateTime.of(2016, 2, 12, 0, 0, 0)));
         super.insert(new Absence(uDAO.getUser(8), aDAO.getAbsenceTypeByID(4), LocalDateTime.of(2016, 12, 25, 10, 30, 0), LocalDateTime.of(2016, 12, 25, 16, 22, 0)));
         super.insert(new Absence(uDAO.getUser(2), aDAO.getAbsenceTypeByID(3), LocalDateTime.of(2016, 8, 17, 0, 0, 0), LocalDateTime.of(2016, 8, 18, 0, 0, 0)));
         super.insert(new Absence(uDAO.getUser(6), aDAO.getAbsenceTypeByID(3), LocalDateTime.of(2016, 8, 26, 12, 30, 0), LocalDateTime.of(2016, 8, 27, 0, 0, 0)));
+        
+        super.insert(new Absence(0, uDAO.getUser(9), aDAO.getAbsenceTypeByID(4), LocalDateTime.of(2016, 8, 18, 8, 0, 0), LocalDateTime.of(2016, 7, 8, 13, 30, 0), "PC exploded BOOM!",true));
+        super.insert(new Absence(0, uDAO.getUser(9), aDAO.getAbsenceTypeByID(4), LocalDateTime.of(2016, 8, 22, 8, 0, 0), LocalDateTime.of(2016, 7, 8, 13, 30, 0), "OH my GOD a TRIPPLE!",true));
+        super.insert(new Absence(0, uDAO.getUser(9), aDAO.getAbsenceTypeByID(3), LocalDateTime.of(2016, 7, 8, 8, 0, 0), LocalDateTime.of(2016, 7, 8, 13, 30, 0), "ITS MA TIME!",true));
+        super.insert(new Absence(0, uDAO.getUser(9), aDAO.getAbsenceTypeByID(4), LocalDateTime.of(2016, 7, 15, 8, 0, 0), LocalDateTime.of(2016, 7, 8, 13, 30, 0), "Sprenggurt",true));
+        super.insert(new Absence(0, uDAO.getUser(9), aDAO.getAbsenceTypeByID(4), LocalDateTime.of(2016, 6, 26, 8, 0, 0), LocalDateTime.of(2016, 7, 8, 13, 30, 0), "ICECREAM I WANT ICECREAM!",true));
         
     }
 
