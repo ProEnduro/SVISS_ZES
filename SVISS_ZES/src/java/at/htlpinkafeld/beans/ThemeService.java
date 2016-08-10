2234a0315e58c5123182ec5576377bdeb74a3d62
ProEnduro <jens.wunderler@gmail.com>
2016-08-10 13:10:33 +0200

    Themes+Update :ideograph_advantage:

--- SVISS_ZES/src/java/at/htlpinkafeld/beans/ThemeService.java
+++ SVISS_ZES/src/java/at/htlpinkafeld/beans/ThemeService.java
@@ -28,7 +28,16 @@ public class ThemeService {
         themes = new ArrayList<>();
         themes.add(new Theme(0, "Standard", "aristo")); 
         themes.add(new Theme(1, "Afterdark", "afterdark"));
-        
+        themes.add(new Theme(2, "Delta", "delta")); 
+        themes.add(new Theme(3, "Trontastic", "trontastic"));
+        themes.add(new Theme(4, "Glass-X", "glass-x"));
+        themes.add(new Theme(5, "Blitzer", "blitzer"));
+        themes.add(new Theme(6, "Le-Frog", "le-frog"));
+        themes.add(new Theme(7, "Redmond", "redmond"));
+        themes.add(new Theme(8, "Cruze", "cruze"));
+        themes.add(new Theme(9, "Eggplant", "eggplant"));
+        themes.add(new Theme(10, "Hot-Sneaks", "hot-sneaks"));
+        themes.add(new Theme(11, "Dot-LUV", "dot-luv"));
         
     }
      
