5554e1595e3a907c8063cce5d9d2a84de9ed4a46
Freya46 <neumeistersebastian@gmx.net>
2016-08-16 13:10:59 +0200

    sollzeit visible in accountverwaltung ->groundwork

--- SVISS_ZES/src/java/at/htlpinkafeld/beans/BenutzerkontoBean.java
+++ SVISS_ZES/src/java/at/htlpinkafeld/beans/BenutzerkontoBean.java
@@ -6,20 +6,26 @@
 package at.htlpinkafeld.beans;
 
 import at.htlpinkafeld.pojo.AccessLevel;
+import at.htlpinkafeld.pojo.SollZeiten;
 import at.htlpinkafeld.pojo.User;
 import at.htlpinkafeld.pojo.UserProxy;
 import at.htlpinkafeld.service.BenutzerverwaltungService;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import at.htlpinkafeld.service.IstZeitService;
+import at.htlpinkafeld.service.SollZeitenService;
+import at.htlpinkafeld.service.TimeConverterService;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
-import java.util.Enumeration;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Properties;
+import javax.annotation.PostConstruct;
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletContext;
+import javax.faces.event.ActionEvent;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
 
 /**
  *
@@ -40,6 +46,17 @@ public class BenutzerkontoBean {
     FacesContext context = FacesContext.getCurrentInstance();
     MasterBean masterBean = (MasterBean) context.getApplication().evaluateExpressionGet(context, "#{masterBean}", MasterBean.class);
     User user;
+
+    ScheduleModel sollzeitModel; 
+    User currentUser;
+    LocalDate pointDate;
+
+    @PostConstruct
+    public void init() {
+        pointDate = LocalDate.of(2016, 8, 1);
+        
+        loadSollZeiten(null);
+    }
 
     public BenutzerkontoBean() {
         user = new UserProxy();
@@ -91,5 +108,38 @@ public class BenutzerkontoBean {
 
     public double getWeekTime() {
         return user.getWeekTime();
+    }
+
+    public ScheduleModel getSollzeitModel() {
+        return sollzeitModel;
+    }
+
+    public void setSollzeitModel(ScheduleModel sollzeitModel) {
+        this.sollzeitModel = sollzeitModel;
+    }
+
+    public void loadSollZeiten(ActionEvent e) {
+        this.sollzeitModel = new DefaultScheduleModel();
+        
+        currentUser = masterBean.getUser();
+        System.out.println(currentUser);
+
+        currentUser = BenutzerverwaltungService.getUser(currentUser.getUsername());
+        System.out.println(currentUser);
+
+        List<SollZeiten> currentSollZeiten;
+
+        currentSollZeiten = SollZeitenService.getSollZeitenByUser(currentUser);
+
+        for (SollZeiten sz : currentSollZeiten) {
+            LocalDate curDate = pointDate.with(TemporalAdjusters.firstInMonth(sz.getDay()));
+            DefaultScheduleEvent de = new DefaultScheduleEvent("", TimeConverterService.convertLocalTimeToDate(curDate, sz.getSollStartTime()),
+                    TimeConverterService.convertLocalDateTimeToDate(LocalDateTime.of(curDate, sz.getSollEndTime())), curDate.getDayOfWeek());
+            sollzeitModel.addEvent(de);
+        }
+    }
+
+    public Date getPointDate() {
+        return IstZeitService.convertLocalDateTimeToDate(pointDate.atStartOfDay());
     }
 }
