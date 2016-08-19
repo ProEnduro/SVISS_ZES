c6a1a91a5dfa3068767b2d23df1a329d52274a67
ProEnduro <jens.wunderler@gmail.com>
2016-08-19 12:25:58 +0200

    abwesenheiten auswertung + excel-icon
    :kimono:

--- SVISS_ZES/src/java/at/htlpinkafeld/beans/alleAbwesenheitenBean.java
+++ SVISS_ZES/src/java/at/htlpinkafeld/beans/alleAbwesenheitenBean.java
@@ -5,16 +5,79 @@
  */
 package at.htlpinkafeld.beans;
 
+import at.htlpinkafeld.pojo.Absence;
+import at.htlpinkafeld.pojo.User;
+import at.htlpinkafeld.service.AbsenceService;
+import at.htlpinkafeld.service.BenutzerverwaltungService;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 /**
  *
  * @author msi
  */
 public class alleAbwesenheitenBean {
-
-    /**
-     * Creates a new instance of alleAbwesenheitenBean
-     */
-    public alleAbwesenheitenBean() {
-    }
+    List<Absence> absence;
+ 
+    User currentUser;
     
+    private String selectedUser;
+    private List<String> allUsers;
+
+    public alleAbwesenheitenBean() {
+        absence=AbsenceService.getAllAbsences();
+        
+        this.loadUserSelect();
+        
+    }
+
+    public List<Absence> getAbsence() {
+        return absence;
+    }
+
+    public void setAbsence(List<Absence> absence) {
+        this.absence = absence;
+    }
+
+    public String getSelectedUser() {
+        return selectedUser;
+    }
+
+    public void setSelectedUser(String selectedUser) {
+        this.selectedUser = selectedUser;
+    }
+
+    public List<String> getAllUsers() {
+        return allUsers;
+    }
+
+    public void setAllUsers(List<String> allUsers) {
+        this.allUsers = allUsers;
+    }
+
+    
+    public String loadUserSelect() {
+        this.selectedUser = "All";
+        this.allUsers = new ArrayList<>();
+
+        allUsers.add("All");
+
+        for (User u : BenutzerverwaltungService.getUserList()) {
+            allUsers.add(u.getUsername());
+        }
+        
+        return "/pages/alleAbwesenheiten.xhtml?faces-redirect=true";
+    }
+
+    public void loadAllAbsenceByUser (){
+        currentUser=BenutzerverwaltungService.getUserByUsername(selectedUser);
+        
+        if(selectedUser.equals("All")){
+            absence=AbsenceService.getAllAbsences();
+        }else{
+            absence=AbsenceService.getAbsenceByUser(currentUser);
+        }
+    }
 }
