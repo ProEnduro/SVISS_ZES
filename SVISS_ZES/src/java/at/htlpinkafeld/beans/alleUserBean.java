83c8a655ec105877e1f4ad8dfba969012cd8e12e
Freya46 <neumeistersebastian@gmx.net>
2016-08-19 08:10:38 +0200

    auswertungsBeans für auswertungs tabs

---
+++ SVISS_ZES/src/java/at/htlpinkafeld/beans/alleUserBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.htlpinkafeld.beans;
+
+/**
+ *
+ * @author msi
+ */
+public class alleUserBean {
+
+    /**
+     * Creates a new instance of alleUserBean
+     */
+    public alleUserBean() {
+    }
+    
+}
---
+++ SVISS_ZES/src/java/at/htlpinkafeld/beans/userDetailsBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.htlpinkafeld.beans;
+
+/**
+ *
+ * @author msi
+ */
+public class userDetailsBean {
+
+    /**
+     * Creates a new instance of userDetailsBean
+     */
+    public userDetailsBean() {
+    }
+    
+}
